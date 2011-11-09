bf75dc47b4ac5e6cde0d645363c8e3ab9a81aa41
Joseph Mordetsky <jmordetsky@gmail.com>
2011-11-09 17:44:27 -0600

    turned linebyline in to a reusable file processor where the processing and output of a line is reusable.

--- lineByline.js
+++ lineByline.js
@@ -3,168 +3,194 @@ var sys = require('util');
 var spawn = require('child_process').spawn;
 var exec = require('child_process').exec;
 
+var defaultDescriptor = {
+	"inputFile":"UsBiz.csv"
+	"outputPrefix":"output",
+	"outputPostFix":".xml",
+	"outputEncoding":"utf-8",
+	"onLine":Csv2Xml,
+	"chunkSize":150000,
+	"startChunk":0,
+	"endChunk":-1,
+	"procs":2,
+	"useHeader":0,
+	"fieldDescriptor":"fields.csv",
+	"onComplete":function() { console.log ("done!"); },
+	"onChunkStart":function(ws){ws.write("<root>", "ascii");},
+	"onChunkEnd":function(ws){ws.write("</root>", "ascii");},
+	"onHeaders":undefined
+}
 
-var file = "UsBiz.csv";
-//var file = "source.csv";
-var desc = "fields.csv";
-var useHeaders = 0;
-var recordsPerFile = 150000;
-var trimQuotes=1;
-var stop = -1;
-var FIXERR = 1; //custom error handling for my file...remove if you don't need.
-var chunk = 0;
-var lineCount = chunk*recordsPerFile;
-var totalExceptions = 0;
-var pathAndPrefix = "temp/output_chunk";
 var pattern = /,(?!(?:[^",]|[^"],[^"])+")/;
-var writeStream;
-
 var fieldDescriptors;
-if (useHeaders==0)
+exports.processFileLineByLine=(descriptor)
 {
-	fieldDescriptors = fs.readFileSync(desc, "ascii");
-	fieldDescriptors = fieldDescriptors.trim().split(pattern);
-} 
+	var chunk =0;
 
+	//merge, use default where undefined
+	for (var attrname in descriptor) { 
+		if (descriptor[attrname]!=undefined)
+		{
+			defaultDescriptor[attrname] = descriptor[attrname]; 
+		}	
+	}
+	
+	if (defaultDescriptor.onLine == undefined) { throw "Requires a line processing function."; }
+	
+	//set up streams
+	makeInputStream(defaultDescriptor.inputFile);
+	
+	var chunk = defaultDescriptor.startChunk;
+	makeOutputStream(defaultDescriptor.outputPrefix + chunk + defaultDescriptor.outputPostFix);
+	if (!defaultDescriptor.useHeaders)
+	{
+		if (defaultDescriptor.fieldDescriptor == undefined) { throw "Either field descriptors must be set or useHeaders must be true"; }
+		fieldDescriptors = fs.readFileSync(defaultDescriptor.fieldDescriptor, "ascii");
+		fieldDescriptors = fieldDescriptors.trim().split(pattern);
+		invoke('onHeaders',fieldDescriptors);
+	}
+		
+}
 
-var count = 0;
-var stream = fs.createReadStream(file);
-stream.on('end', function()
+function invoke(name,params)
 {
-	console.log("read stream closing.");
-	writeStream.write("</root>", "ascii");
-	writeStream.end();
-	writeStream.destroySoon();
-});
+		if (defaultDescriptor[name] != undefined)
+		{
+			defaultDescriptor[name](params);
+		}
+}
 
-function makeWriteStream()
+var stream;
+function makeInputStream(inputFile)
+{
+	stream = fs.createReadStream(inputFile);
+	var buffer = "";
+	stream.on('end', function()
+	{
+		console.log("read stream closing.");
+		invokeOnChunkEnd();
+		writeStream.end();
+		writeStream.destroySoon();
+		invoke('onComplete');
+	});
+	
+	stream.on('data', function(data){
+		buffer+=data;
+	    var index = buffer.indexOf('\n');
+	    while (index > -1) {
+	      var line = buffer.substring(0, index);
+	      buffer = buffer.substring(index + 1);
+	      parseLine(line);
+	      index = buffer.indexOf('\n');
+	    }
+	});
+	
+}
+
+var writeStream;
+function makeOutputStream()
 {
 		writeStream = fs.createWriteStream(pathAndPrefix + chunk + ".xml");	
 		writeStream.on('drain', function(){
 			console.log("drain");
 			stream.resume();
-
 		});
-		writeStream.write("<root>", "ascii");
+		invoke('onChunkStart',writeStream);
 }
 
-makeWriteStream();
-
-var buffer = "";
-
-stream.on('data', function(data){
-	console.log("data");
-	buffer+=data;
-    var index = buffer.indexOf('\n');
-    while (index > -1) {
-      var line = buffer.substring(0, index);
-      buffer = buffer.substring(index + 1);
-      parseLine(line);
-      index = buffer.indexOf('\n');
-    }
-});
-	
 function parseLine(line)
 {
-		console.log(lineCount);
 		if (useHeaders == 1 && lineCount == 0)
 		{
 			//parse headers
 			fieldDescriptors = line.split(pattern);
+			invoke('onHeaders',writeStream);;
 			lineCount++;
 			return;
 		}
 		
-		var xml = CsvToXML([line], chunk);
+		
+		var record = defaultDescriptor.onLine(fieldDescriptors, line);
 		lineCount++;
 		count++;
-		if (!writeStream.write(xml, "ascii"))
+	
+		if (!writeStream.write(record, "ascii"))
 		{
 			stream.pause();
 		}
 	
-		if (count>=recordsPerFile)
+		if (count>=defaultDescriptor.recordsPerFile)
 		{
-			
-			console.log("cutting file.");
-			writeStream.write("</root>", "ascii");	
+				
+			invoke('onChunkEnd',writeStream);
 			writeStream.end();
 			writeStream.destroySoon();
 			chunk++;
 			count = 0;
 			makeWriteStream();
-			
 		}
 	
 }
 
-
-function CsvToXML(data)
+var FIXERR = 1;
+function CsvToXML(line)
 {
 	
- 	var lines = data;
 	var builder = require('xmlbuilder');
 	var doc = builder.create();
 
 	var dupeColhandler = {};	
-	for(var i =0; i<lines.length;i++)
+	var cols = line.trim().split(pattern);
+
+	//custom error handler, set FIXERR to 0 and ignore.
+	if (FIXERR && cols.length == 429)
 	{
-		var cols = lines[i].trim().split(pattern);
-		
-		//custom error handler, set FIXERR to 0 and ignore.
-		if (FIXERR && cols.length == 429)
-		{
-			//colapse fields
-			//console.log("Fixing name issue: " + cols[72] + ", " + cols[73] );
-			cols[72] = cols[72] + ", " + cols[73]; 
-			cols.splice(73,1);
-			//console.log("New val: " + cols[72]);
-		}
+		//colapse fields
+		//console.log("Fixing name issue: " + cols[72] + ", " + cols[73] );
+		cols[72] = cols[72] + ", " + cols[73]; 
+		cols.splice(73,1);
+		//console.log("New val: " + cols[72]);
+	}
 
-		if (cols.length == fieldDescriptors.length)
+	if (cols.length == fieldDescriptors.length)
+	{
+		var root = doc.begin("row");
+		for(var ii=0;ii<cols.length;ii++)
 		{
-			var root = doc.begin("row");
-			for(var ii=0;ii<cols.length;ii++)
+
+			if (fieldDescriptors[ii] != undefined )
 			{
-
-				if (fieldDescriptors[ii] != undefined )
-				{
-					if (cols[ii] == ""){
-						cols[ii] = "\t";
-					}
-					if (trimQuotes == 1)
-					{
-						cols[ii]=cols[ii].replace(/\"/g, "");
-					}
-					
-					var colName = fieldDescriptors[ii];
-					var colNameCheck = colName;
-					var count = 1;
-					if(dupeColhandler[colName]!=undefined)
-					{
-						while(dupeColhandler[colNameCheck]!=undefined){
-							colNameCheck = colName + count;
-							count++; 
-						}
-						
-						root.ele(colNameCheck).txt(cols[ii]);	
-						dupeColhandler[colNameCheck] = 1;		
-					}
-					else
-					{
-						root.ele(colName).txt(cols[ii]);	
-						dupeColhandler[colName] = 1;
-					}
-					
-					
+				if (cols[ii] == ""){
+					cols[ii] = "\t";
 				}
-			}	
-		}
-	
+				if (trimQuotes == 1)
+				{
+					cols[ii]=cols[ii].replace(/\"/g, "");
+				}
+
+				var colName = fieldDescriptors[ii];
+				var colNameCheck = colName;
+				var count = 1;
+				if(dupeColhandler[colName]!=undefined)
+				{
+					while(dupeColhandler[colNameCheck]!=undefined){
+						colNameCheck = colName + count;
+						count++; 
+					}
+
+					root.ele(colNameCheck).txt(cols[ii]);	
+					dupeColhandler[colNameCheck] = 1;		
+				}
+				else
+				{
+					root.ele(colName).txt(cols[ii]);	
+					dupeColhandler[colName] = 1;
+				}
+			}
+		}	
 	}
 	
-	//var ret = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh";
-	//ret = ret + ret + ret + ret + ret + ret + ret + ret + ret + ret + ret;
+	
 	var ret = doc.toString({pretty:true});
 	doc = null;
 	builder = null;
